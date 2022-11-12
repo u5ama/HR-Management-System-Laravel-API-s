b306b462bfc56c3ed1ab84f2ebfd7a7a523fc114
Usama Aslam <u.aslam105@gmail.com>
2022-11-12 18:56:07 +0500

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name')->nullable();
             $table->enum('user_type',['company','staff'])->comment('User Role');
             $table->enum('user_status',['active','NotActive'])->comment('User Status');
             $table->integer('staff_id')->nullable();
